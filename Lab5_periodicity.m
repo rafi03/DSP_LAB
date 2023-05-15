c408ee32f5c0706e9c1ce4afcd4edb9987a292ae
rafi03 <alrafikp@gmail.com>
2023-05-15 15:18:11 +0600

    Update Lab5_periodicity.m

--- Lab5_periodicity.m
+++ Lab5_periodicity.m
@@ -11,25 +11,3 @@ r = xcorr(x);
 subplot(2,1,2);
 plot(r);
 title("Auto-correlation");
-
-% maxim = -1;
-% indx = -1;
-% for i=1:length(r)
-%     if r(1,i) > maxim
-%         maxim = r(1,i);
-%         indx = i;
-%     end
-% end
-% disp(r)
-% disp(answ)
-%disp(indx-40-1);
-% subplot(3,1,3);
-% plot(-40:1:40,r);
-% title("Correlation");
-% xlim([1 n+(n2-1)]);
-% title("Built in");
-% 
-% subplot(4,1,4);
-% stem(answ,'filled');
-% xlim([1 n+(n2-1)]);
-% title("Signal");
